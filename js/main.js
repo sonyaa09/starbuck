911afa005670e4fe4e4b4d678f94a517359ca8b3
Sonya <i_sony_a@naver.com>
2022-03-03 14:17:16 +0900

    Ch 10. 스타벅스 예제 19. 요소 슬라이드 - 프로모션 이미지 슬라이드 (1)

---
+++ js/main.js
@@ -0,0 +1,91 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+
+
+const badgeEl = document.querySelector('header .badges');
+
+//  _.throttle(함수, 시간)  //
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        //gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        //  배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
+
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, 
+        opacity: 1
+    });
+});
+
+
+/**
+ * 슬라이드 요소 관리
+ */
+ new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical', // 수직 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true // 반복 재생 여부
+  })
+  new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: { // 자동 재생 여부
+      delay: 5000 // 5초마다 슬라이드 바뀜
+    },
+    loop: true, // 반복 재생 여부
+    slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    pagination: { // 페이지 번호 사용 여부
+      el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+      clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+    }
+  })
+  new Swiper('.awards .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+    spaceBetween: 30, // 슬라이드 사이 여백
+    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+    // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    }
+  })
+  
+
