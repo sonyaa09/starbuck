342b8e90b2209395dfef8b6dec34e225bd9837b6
Sonya <i_sony_a@naver.com>
2022-03-04 12:06:02 +0900

    Ch 10. 스타벅스 예제 32. 페이지 상단으로 이동(ScrollTo)

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ searchInputEl.addEventListener('blur', function(){
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top')
 
 //  _.throttle(함수, 시간)  //
 window.addEventListener('scroll', _.throttle(function () {
@@ -29,15 +30,28 @@ window.addEventListener('scroll', _.throttle(function () {
             opacity: 0,
             display: 'none'
         });
+        // 버튼 보이기
+        gsap.to(toTopEl, .2, {
+          x: 0
+        });
     } else {
-        //  배지 보이기
+        // 배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         });
+        // 버튼 숨기기
+        gsap.to(toTopEl, .2, {
+          x: 100
+        });
     }
 }, 300));
 
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+})
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
