8415b1749497da2b1a0b8e6d6b47b6be6b9d4ba8
Sonya <i_sony_a@naver.com>
2022-03-04 11:19:03 +0900

    Ch 10. 스타벅스 예제 30. 다중 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ fadeEls.forEach(function (fadeEl, index) {
     direction: 'vertical', // 수직 슬라이드
     autoplay: true, // 자동 재생 여부
     loop: true // 반복 재생 여부
-  })
+  });
   new Swiper('.promotion .swiper-container', {
     // direction: 'horizontal', // 수평 슬라이드
     autoplay: { // 자동 재생 여부
@@ -74,19 +74,18 @@ fadeEls.forEach(function (fadeEl, index) {
       prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
       nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
     }
-  })
+  });
   new Swiper('.awards .swiper-container', {
-    // direction: 'horizontal', // 수평 슬라이드
-    autoplay: true, // 자동 재생 여부
-    loop: true, // 반복 재생 여부
-    spaceBetween: 30, // 슬라이드 사이 여백
-    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
-    // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
-    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-      prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
-      nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+      prevEl: '.awards .swiper-prev',
+      nextEl: '.awards .swiper-next'
     }
-  })
+  });
+
   
 
 const promotionEl = document.querySelector('.promotion');
